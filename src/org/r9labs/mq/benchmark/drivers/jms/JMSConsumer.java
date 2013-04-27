2b524c70df060d34e10648e81be47665086640b9
Jason Barto <jason.p.barto@gmail.com>
2013-04-27 22:51:12 +0100

    Added placeholders for future JMS driver.

---
+++ src/org/r9labs/mq/benchmark/drivers/jms/JMSConsumer.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.r9labs.mq.benchmark.drivers.jms;
+
+import org.r9labs.mq.benchmark.drivers.ConsumingDriver;
+
+/**
+ *
+ * @author jpbarto
+ */
+public class JMSConsumer implements ConsumingDriver {
+
+    @Override
+    public void start() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public byte[] getMessage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
