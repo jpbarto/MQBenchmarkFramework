55efbdfee849a50a0ace729dcfbd327952cf3ef3
Jason Barto <jason.p.barto@gmail.com>
2013-04-28 00:09:39 +0100

    Added initial JMS driver capability, tested using topics only, and against ActiveMQ

--- src/org/r9labs/mq/benchmark/drivers/jms/JMSConsumer.java
+++ src/org/r9labs/mq/benchmark/drivers/jms/JMSConsumer.java
@@ -4,6 +4,20 @@
  */
 package org.r9labs.mq.benchmark.drivers.jms;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jms.BytesMessage;
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.Topic;
+import javax.jms.TopicConnection;
+import javax.jms.TopicConnectionFactory;
+import javax.jms.TopicSession;
+import javax.jms.TopicSubscriber;
+import javax.naming.Context;
+import javax.naming.NamingException;
 import org.r9labs.mq.benchmark.drivers.ConsumingDriver;
 
 /**
@@ -12,19 +26,94 @@ import org.r9labs.mq.benchmark.drivers.ConsumingDriver;
  */
 public class JMSConsumer implements ConsumingDriver {
 
+    private String username = null;
+    private String password = null;
+    private String topicName = null;
+    private Topic topic = null;
+    private String queueName = null;
+    private Queue queue = null;
+    private Context context = null;
+    private TopicSession session = null;
+    private TopicConnection conn = null;
+    private TopicSubscriber cin = null;
+
+    public JMSConsumer(Context context) {
+        this.context = context;
+    }
+
+    public void setLogin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setTopic(String topic) {
+        topicName = topic;
+    }
+
+    public void setQueue(String queue) {
+        queueName = queue;
+    }
+
     @Override
     public void start() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       if (topicName != null) {
+            TopicConnectionFactory connF;
+            try {
+                connF = (TopicConnectionFactory) context.lookup("ConnectionFactory");
+            } catch (NamingException ex) {
+                Logger.getLogger(JMSProducer.class.getName()).log(Level.SEVERE, "Error retrieving connection factory from context", ex);
+                return;
+            }
+
+            try {
+                if (username != null) {
+                    conn = connF.createTopicConnection(username, password);
+                } else {
+                    conn = connF.createTopicConnection();
+                }
+                conn.start();
+                session = conn.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+                topic = session.createTopic(topicName);
+                cin = session.createSubscriber(topic);
+            } catch (JMSException ex) {
+                Logger.getLogger(JMSProducer.class.getName()).log(Level.SEVERE, "Error creating message consumer", ex);
+            }
+        }
     }
 
     @Override
     public void stop() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            cin.close();
+            session.close();
+            conn.stop();
+            conn.close();
+        } catch (JMSException ex) {
+            Logger.getLogger(JMSProducer.class.getName()).log(Level.WARNING, "An error occurred stopping consumer connection to broker", ex);
+        }
     }
 
     @Override
     public byte[] getMessage() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        BytesMessage msg;
+        try {
+            msg = (BytesMessage) cin.receive(500);
+        } catch (JMSException ex) {
+            Logger.getLogger(JMSConsumer.class.getName()).log(Level.SEVERE, "Error receiving BytesMessage", ex);
+            return null;
+        }
+        
+        if (msg != null) {
+            try {
+                byte[] ret = new byte[(int)msg.getBodyLength()];
+                msg.readBytes(ret);
+                msg.acknowledge();
+                return ret;
+            } catch (JMSException ex) {
+                Logger.getLogger(JMSConsumer.class.getName()).log(Level.SEVERE, "Error reading received BytesMessage", ex);
+            }
+        }
+        
+        return null;
     }
-    
 }
--- src/org/r9labs/mq/benchmark/drivers/jms/JMSDriverFactory.java
+++ src/org/r9labs/mq/benchmark/drivers/jms/JMSDriverFactory.java
@@ -5,6 +5,11 @@
 package org.r9labs.mq.benchmark.drivers.jms;
 
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import org.r9labs.mq.benchmark.drivers.ConsumingDriver;
 import org.r9labs.mq.benchmark.drivers.DriverFactory;
 import org.r9labs.mq.benchmark.drivers.ProducingDriver;
@@ -14,10 +19,31 @@ import org.r9labs.mq.benchmark.drivers.ProducingDriver;
  * @author jpbarto
  */
 public class JMSDriverFactory implements DriverFactory {
-
+    private Context context = null;
+    private String brokerUsername;
+    private String brokerPassword;
+    
+    private String ptopic;
+    private String pqueue;
+    
+    private String ctopic;
+    private String cqueue;
+    
     @Override
-    public void initialize(Properties p) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void initialize(Properties properties) {
+        brokerUsername = (properties.containsKey("jmsfactory.broker.username")) ? properties.getProperty("jmsfactory.broker.username") : null;
+        brokerPassword = (properties.containsKey("jmsfactory.broker.password")) ? properties.getProperty("jmsfactory.broker.password") : null;
+        
+        ptopic = (properties.containsKey("jmsfactory.producer.topic")) ? properties.getProperty("jmsfactory.producer.topic") : null;
+        pqueue = (properties.containsKey("jmsfactory.producer.queue")) ? properties.getProperty("jmsfactory.producer.queue") : null;
+        
+        ctopic = (properties.containsKey("jmsfactory.consumer.topic")) ? properties.getProperty("jmsfactory.consumer.topic") : null;
+        cqueue = (properties.containsKey("jmsfactory.consumer.queue")) ? properties.getProperty("jmsfactory.consumer.queue") : null;
+        try {
+            context = new InitialContext (properties);
+        } catch (NamingException ex) {
+            Logger.getLogger(JMSDriverFactory.class.getName()).log(Level.SEVERE, "Error creating InitialContext from properties", ex);
+        }      
     }
 
     @Override
@@ -27,12 +53,36 @@ public class JMSDriverFactory implements DriverFactory {
 
     @Override
     public ProducingDriver createProducingDriver() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        JMSProducer ret = new JMSProducer (context);
+        if (brokerUsername != null) {
+            ret.setLogin (brokerUsername, brokerPassword);
+        }
+        
+        if (pqueue != null) {
+            ret.setQueue (pqueue);
+        }
+        
+        if (ptopic != null) {
+            ret.setTopic (ptopic);
+        }
+        return ret;
     }
 
     @Override
-    public ConsumingDriver createConsumingDriver() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ConsumingDriver createConsumingDriver() {        
+        JMSConsumer ret = new JMSConsumer (context);
+        if (brokerUsername != null) {
+            ret.setLogin (brokerUsername, brokerPassword);
+        }
+        
+        if (cqueue != null) {
+            ret.setQueue (cqueue);
+        }
+        
+        if (ctopic != null) {
+            ret.setTopic (ctopic);
+        }
+        return ret;
     }
     
 }
